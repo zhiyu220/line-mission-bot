3f690c1a451c2e17d24845b8d728c87731090039
zhiyu220 <yulin8902@gmail.com>
2025-04-18 10:08:49 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -26,9 +26,274 @@ handler = WebhookHandler(CHANNEL_SECRET)
 
 # 社團對照表（代替資料庫簡化版）
 club_mapping = {
-    "a8f2XjL9zQ": {"name": "吉他社", "type": "康樂性"},
-    "entry_start": {"name": "入口說明", "type": "entry"}
-    # ... 其他亂碼對照
+  "W9ZplJPSRB": {
+    "name": "AIESEC元智分會",
+    "type": "學術文藝性"
+  },
+  "Zj4MEBEGFC": {
+    "name": "攝影社",
+    "type": "學術文藝性"
+  },
+  "tkJy7A81c8": {
+    "name": "圍棋社",
+    "type": "學術文藝性"
+  },
+  "RDPN7sEVML": {
+    "name": "證券研習社",
+    "type": "學術文藝性"
+  },
+  "pdBPIob4yM": {
+    "name": "CPA Club",
+    "type": "學術文藝性"
+  },
+  "0iFVTCcJOC": {
+    "name": "烘焙社",
+    "type": "學術文藝性"
+  },
+  "Taj47ICVfu": {
+    "name": "新世紀領袖培育計畫團",
+    "type": "學術文藝性"
+  },
+  "QLfeYD3c4O": {
+    "name": "資訊研究社",
+    "type": "學術文藝性"
+  },
+  "EOakHXDGRn": {
+    "name": "桌上遊戲社",
+    "type": "學術文藝性"
+  },
+  "GtCgBRjeWR": {
+    "name": "自造者社(機器人研究社)",
+    "type": "學術文藝性"
+  },
+  "PRmNJDYeGe": {
+    "name": "遊戲創作社",
+    "type": "學術文藝性"
+  },
+  "ADW4av1WYR": {
+    "name": "管理顧問社",
+    "type": "學術文藝性"
+  },
+  "NUo0Bf4zg6": {
+    "name": "卡牌社",
+    "type": "學術文藝性"
+  },
+  "wDNaFjpIZW": {
+    "name": "競技程式人才培育社",
+    "type": "學術文藝性"
+  },
+  "QSoGJkXdod": {
+    "name": "生命泉學生教會",
+    "type": "聯誼性"
+  },
+  "H0jB8cJX6D": {
+    "name": "越南學生聯誼會",
+    "type": "聯誼性"
+  },
+  "lWLqJz12Fl": {
+    "name": "閱人社",
+    "type": "聯誼性"
+  },
+  "BHkYWnaYOP": {
+    "name": "原青社",
+    "type": "聯誼性"
+  },
+  "DG4kY5PU5l": {
+    "name": "光鹽唱詩社",
+    "type": "聯誼性"
+  },
+  "Z0JkB258bG": {
+    "name": "熱舞社",
+    "type": "康樂性"
+  },
+  "u0WRpgXjnT": {
+    "name": "流行音樂錄影帶舞蹈社",
+    "type": "康樂性"
+  },
+  "bfBfvSEXPg": {
+    "name": "MIDI熱音社",
+    "type": "康樂性"
+  },
+  "aQMDrPdooj": {
+    "name": "管樂社",
+    "type": "康樂性"
+  },
+  "2oxuR9lKUa": {
+    "name": "吉他社",
+    "type": "康樂性"
+  },
+  "CyF4b9tSAw": {
+    "name": "弦樂社",
+    "type": "康樂性"
+  },
+  "BFt6t4aoz2": {
+    "name": "流行音樂錄音社",
+    "type": "康樂性"
+  },
+  "5nvb8Mv0uB": {
+    "name": "鋼琴社",
+    "type": "康樂性"
+  },
+  "CYWDMBJx03": {
+    "name": "Disc Jockey 社",
+    "type": "康樂性"
+  },
+  "D6QSExxBR7": {
+    "name": "嘻哈文化研究社",
+    "type": "康樂性"
+  },
+  "gCEj6E7lPb": {
+    "name": "崇德青年志工社",
+    "type": "服務性"
+  },
+  "x1Lpx1pYqP": {
+    "name": "動物關懷社",
+    "type": "服務性"
+  },
+  "G6AgY9Qjwg": {
+    "name": "中智社",
+    "type": "服務性"
+  },
+  "9L2w47ftRy": {
+    "name": "IC部落社",
+    "type": "服務性"
+  },
+  "k9cQvY4nIf": {
+    "name": "跆拳道社",
+    "type": "體育性"
+  },
+  "okNf4twVP4": {
+    "name": "柔道社",
+    "type": "體育性"
+  },
+  "OjAy2nzG7k": {
+    "name": "劍道社",
+    "type": "體育性"
+  },
+  "dhi5vl5gco": {
+    "name": "棒球社",
+    "type": "體育性"
+  },
+  "br9CkTHFz8": {
+    "name": "排球社",
+    "type": "體育性"
+  },
+  "xUnFtNCwvL": {
+    "name": "長拳社",
+    "type": "體育性"
+  },
+  "tRQUSEcOxa": {
+    "name": "滑板社",
+    "type": "體育性"
+  },
+  "kwNkPodduo": {
+    "name": "登山社",
+    "type": "體育性"
+  },
+  "nZkQKZnwTN": {
+    "name": "足球社",
+    "type": "體育性"
+  },
+  "VBv4xHvOki": {
+    "name": "擊劍社",
+    "type": "體育性"
+  },
+  "SI1jY5muMw": {
+    "name": "學生會",
+    "type": "自治性"
+  },
+  "fwD05vrbyf": {
+    "name": "電機系乙組學會",
+    "type": "自治性"
+  },
+  "Ql7yImK8xK": {
+    "name": "電機系丙組學會",
+    "type": "自治性"
+  },
+  "wpBbptBrbe": {
+    "name": "電通英專班學會",
+    "type": "自治性"
+  },
+  "Ufh3C5edFp": {
+    "name": "化材系學會",
+    "type": "自治性"
+  },
+  "cvdLZpgsaB": {
+    "name": "工管系學會",
+    "type": "自治性"
+  },
+  "MOFOBNDITs": {
+    "name": "資工系學會",
+    "type": "自治性"
+  },
+  "zEasvAwnbY": {
+    "name": "資管系學會",
+    "type": "自治性"
+  },
+  "JH8vpMxbSP": {
+    "name": "資傳系學會",
+    "type": "自治性"
+  },
+  "1Jx3PTFOCn": {
+    "name": "資訊英專班學會",
+    "type": "自治性"
+  },
+  "zFonEZlDyE": {
+    "name": "財金班學會",
+    "type": "自治性"
+  },
+  "iQqMqd26aF": {
+    "name": "國企班學會",
+    "type": "自治性"
+  },
+  "Wy1Ze66Pcb": {
+    "name": "會計班學會",
+    "type": "自治性"
+  },
+  "NVNuAYpBfd": {
+    "name": "管理學院英語專班",
+    "type": "自治性"
+  },
+  "8QGYDwDvhK": {
+    "name": "應外系學會",
+    "type": "自治性"
+  },
+  "gdfCZ9n2XM": {
+    "name": "中語系學會",
+    "type": "自治性"
+  },
+  "hpnIxo4Ewn": {
+    "name": "人社英專班學會",
+    "type": "自治性"
+  },
+  "As7HI2w4oY": {
+    "name": "男生宿舍自治會",
+    "type": "自治性"
+  },
+  "GuPTGvlOgb": {
+    "name": "女生宿舍自治會",
+    "type": "自治性"
+  },
+  "18fRsdk2hO": {
+    "name": "輔導義工",
+    "type": "義工性"
+  },
+  "bS8YVDLaUo": {
+    "name": "衛保義工",
+    "type": "義工性"
+  },
+  "8vkD9MeTH9": {
+    "name": "生活尖兵",
+    "type": "義工性"
+  },
+  "eiTtQeHumP": {
+    "name": "YOUTH志工大聯盟",
+    "type": "義工性"
+  },
+  "FmzjiGyhDB": {
+    "name": "元智大使",
+    "type": "義工性"
+  }
 }
 
 @app.route("/callback", methods=['POST'])
